4ca9a94177ce4569c98b00ee8aabd9665a9bfb23
Alicja Kozikowska <alakozikowska@gmail.com>
2017-10-17 01:21:30 +0200

    Add admin interface for models

--- companysite/roombook/admin.py
+++ companysite/roombook/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event, Room, RoomReservation
+
+
+for model in (Event, Room, RoomReservation):
+    admin.site.register(model)
--- companysite/roombook/models.py
+++ companysite/roombook/models.py
@@ -8,12 +8,18 @@ class Room(models.Model):
     name = models.CharField(max_length=50)
     capacity = models.IntegerField()
 
+    def __str__(self):
+        return 'Room {} {}'.format(self.symbol, self.name)
+
 
 class RoomReservation(models.Model):
 
     room = models.ForeignKey(Room, on_delete=models.PROTECT)
     start = models.DateTimeField()
     end = models.DateTimeField()
+
+    def __str__(self):
+        return '{}: {}'.format(self.room, self.start)
 
 
 class Event(models.Model):
@@ -34,3 +40,6 @@ class Event(models.Model):
     end = models.DateTimeField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default=MEETING)
     room_reservation = models.OneToOneField(RoomReservation, null=True, blank=True)
+
+    def __str__(self):
+        return '{}: {}'.format(self.title, self.start)
